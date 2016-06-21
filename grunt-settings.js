a37babf2d4fa515d324d671079b99da7e76e4e98
Marmik Desai <marmikdesai@live.com>
2016-06-21 10:28:42 +0530

    sample html without sass
    sample html without sass

---
+++ grunt-settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+    cssDir:           'css/',
+    cssMainFileDir:   'css/',
+    cssMainFileName:  'main',
+    jsDir:            'js/',
+    imgDir:           'images/',
+    imgSourceDir:     'sourceimages/',
+    //hasBower: {includePaths: ['bower_components/foundation/scss']},
+
+    // sftp server
+    sftpServer:       'example.com',
+    sftpPort:         '2121',
+    sftpLogin:        'login',
+    sftpPas:          'password',
+    sftpDestination:  '/pathTo/css'
+  };
